662f6c75ebf85596a690b79be49cdd1d878b5272
Dan Njoku <dnjoku@ups.com>
2021-11-30 11:46:33 -0500

    test budgetlist git push origin master

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,3 +1,9 @@
 
 class BudgetList():
-    
+    def __init__(self, budget):
+        self.budget = budget
+        self.sum_expenses = 0
+        self.expenses = 0
+        self.sum_overages = 0
+        self.overages = []
+        
