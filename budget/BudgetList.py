1345ff0ea9cb23e721f967d3b05908d36dfc96f4
Dan Njoku <dnjoku@ups.com>
2021-11-30 11:48:37 -0500

    nitialize sum_expenses to 0 and expenses to an empty list in __init__()

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -3,7 +3,6 @@ class BudgetList():
     def __init__(self, budget):
         self.budget = budget
         self.sum_expenses = 0
-        self.expenses = 0
+        self.expenses = []
         self.sum_overages = 0
         self.overages = []
-        
