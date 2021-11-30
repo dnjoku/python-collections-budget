4ecf67f13f4354eaf8eee9c1168dde7ad63c8102
Dan Njoku <dnjoku@ups.com>
2021-11-30 11:18:46 -0500

    plot test

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,6 +1,8 @@
 from . import Expense
 import collections
 
+import matplotlib.pyplot as plt # Remember need to pip install matplotlib
+
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
 spending_categories = []
@@ -9,3 +11,11 @@ for expense in expenses.list:
 
 spending_counter = collections.Counter(spending_categories)
 top5 = spending_counter.most_common(5)
+categories, count = zip(*top5)
+
+
+# Graph each spending category count as a bar chart
+fix, ax - plt.subplots()
+ax.bar(categories, count)
+ax.set_title('# of Purchases by Category')
+plt.show()
