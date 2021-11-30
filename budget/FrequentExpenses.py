22b1b149429cc7cf6a4db1cea86b66e1733f1016
Dan Njoku <dnjoku@ups.com>
2021-11-30 10:55:18 -0500

    spending counter test

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,11 +1,2 @@
 from . import Expense
 import collections
-
-expenses = Expense.Expenses()
-expenses.read_expense('data/spending_data.csv')
-spending_categories = []
-    for expense in expenses.list:
-        spending_categories.append(expense.category)
-
-spending_counter = collections.Counter(spending_categories)
-print(spending_counter)
