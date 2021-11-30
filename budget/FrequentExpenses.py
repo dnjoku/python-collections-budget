d74451cb3098e78a3bffbe062dc4df0906acbe5a
Dan Njoku <dnjoku@ups.com>
2021-11-30 11:20:52 -0500

    plot fixed

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -12,3 +12,9 @@ for expense in expenses.list:
 spending_counter = collections.Counter(spending_categories)
 top5 = spending_counter.most_common(5)
 categories, count = zip(*top5)
+
+# Graph each spending category count as a bar chart
+fix, ax = plt.subplots()
+ax.bar(categories, count)
+ax.set_title('# of Purchases by Category')
+plt.show()
