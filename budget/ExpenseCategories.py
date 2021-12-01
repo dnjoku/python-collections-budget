24c3652cd639ca6c5a3f55297273de549cc95589
Dan Njoku <dnjoku@ups.com>
2021-12-01 09:21:56 -0500

    Timing set comprehension completed

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -26,7 +26,18 @@ expenses.read_expenses('data/spending_data.csv')
     number=100000,
     globals=globals()
     ))
-    
+
+    print(timeit.timeit(stmt="expenses.categorize_set_comprehension()",
+    setup=
+    '''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+    ''',
+    number=100000,
+    globals=globals()
+    ))
+
     fig, ax = plt.subplots()
     labels = ['Necessary', 'Food', 'Unnecessary']
 
